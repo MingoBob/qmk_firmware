f204d5486fb7d35aa06994449065bb2e18467b11
Brian (Silverback) Block <Brian.Block@dieboldnixdorf.com>
2018-07-16 17:15:18 -0400

    Updating the wireing of the LAYOUT macro.
    It turns out that the LAYOUT macro which used to be called KEYMAP
    changed around some of the terminonlogy about how the columns were
    numbered.  This in turn changed how the existing key mapping worked with
    the new firmware.  I have altered the layout to correctly model my
    column wiring and moved around the key mappings back to what I was
    expecing.

--- keyboards/handwired/dactyl_manuform/keymaps/default/keymap.c
+++ keyboards/handwired/dactyl_manuform/keymaps/default/keymap.c
@@ -42,13 +42,13 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  */
 
 [_BASE] = LAYOUT( \
-  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,                                     KC_Y,    KC_U,    KC_I,    KC_O,    KC_P, \
+  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,                                     KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    \
   KC_A,    KC_S,    KC_D,    KC_F,    KC_G,                                     KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, \
   KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,                                     KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, \
   KC_LEFT,  KC_RIGHT,                                                             KC_UP, KC_DOWN, \
-                                 ALT_T(KC_BSPC), SFT_T(KC_DEL),   LGUI_T(KC_ENT), CTL_T(KC_SPC),       \
-                                 CTL_T(KC_TAB),  KC_HOME, KC_END,  KC_ESC,                   \
-  LT(_NAV, KC_TRNS), KC_GRV,  KC_BSLS, LT(_NUM, KC_QUOT)                  \
+  ALT_T(KC_BSPC), SFT_T(KC_DEL),   LGUI_T(KC_ENT), CTL_T(KC_SPC),       \
+  LT(_NAV, KC_TRNS), CTL_T(KC_TAB) , KC_ESC, LT(_NUM, KC_QUOT),         \
+  KC_GRV, KC_HOME, KC_END, KC_BSLS                                      \
     ),
 
 /* Navigation layer
@@ -107,8 +107,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   KC_EXLM,  KC_AT,  KC_HASH,  KC_DLR, KC_PERC, KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, \
             KC_MINUS,  KC_EQUAL,                                     KC_LBRC, KC_RBRC, \
                              KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
-                             KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
-                             KC_F11, KC_F12, KC_TRNS, KC_TRNS
+                             KC_F11, KC_TRNS, KC_TRNS, KC_TRNS,
+                             KC_F12, KC_TRNS, KC_TRNS, KC_TRNS
     )
 };
 

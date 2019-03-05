4a4f3550013d5ee02ab68f142454168b4989ba15
Robert Konecny <RKonecny@drbsystems.com>
2019-03-05 11:31:42 -0500

    Switched the keymapping for GUI and ALT

--- keyboards/handwired/dactyl_manuform/keymaps/default/keymap.c
+++ keyboards/handwired/dactyl_manuform/keymaps/default/keymap.c
@@ -46,7 +46,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   KC_A,    KC_S,    KC_D,    KC_F,    KC_G,                                     KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, \
   KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,                                     KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, \
   KC_LEFT,  KC_RIGHT,                                                             KC_UP, KC_DOWN, \
-  ALT_T(KC_BSPC), SFT_T(KC_DEL),   LGUI_T(KC_ENT), CTL_T(KC_SPC),       \
+  SFT_T(KC_BSPC), ALT_T(KC_DEL),   LGUI_T(KC_ENT), CTL_T(KC_SPC),       \
   LT(_NAV, KC_TRNS), CTL_T(KC_TAB) , KC_ESC, LT(_NUM, KC_QUOT),         \
   KC_GRV, KC_HOME, KC_END, KC_BSLS                                      \
     ),
